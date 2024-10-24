2608d77b31befdad6ac1d9f4815ad49ca01a38e0
Roman Smelov <truejaba@protonmail.com>
2024-10-24 19:28:37 +0300

    'initial'

---
+++ src/main/java/edu/telegrambot/TelegramBotGetApplication.java
@@ -0,0 +1,14 @@
+package edu.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+
+
+@SpringBootApplication
+public class TelegramBotGetApplication {
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(TelegramBotGetApplication.class, args);
+
+    }
+}
